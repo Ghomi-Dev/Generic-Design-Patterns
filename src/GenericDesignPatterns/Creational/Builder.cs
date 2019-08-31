a89e7b6e3c66f83d94d4f90beaa1c4d9da01b0a8
Seyed Abbas Ghomi <seyedabbas.ghomi@protonmail.com>
2019-09-01 01:38:01 +0430

    Generic Builder Pattern implemented

---
+++ src/GenericDesignPatterns/Creational/Builder.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace GenericDesignPatterns.Creational
+{
+    public class Builder<T>
+    where T : class
+    {
+        private class PropertyWrapper
+        {
+            public PropertyInfo Property { get; }
+            public object Value { get; set; }
+
+            internal PropertyWrapper(PropertyInfo property, object value)
+            {
+                Property = property;
+                Value = value;
+            }
+        }
+
+        private readonly IDictionary<string, PropertyWrapper> _propertiesInfo = new Dictionary<string, PropertyWrapper>();
+
+        public Builder()
+        {
+            PropertyInfo[] properties = typeof(T).GetProperties();
+            foreach (PropertyInfo p in properties)
+            {
+                _propertiesInfo.Add(p.Name, new PropertyWrapper(p, null));
+            }
+        }
+
+        public Builder<T> SetProperty<TValue>(
+            Expression<Func<T, TValue>> property, TValue value)
+        {
+            var body = property.Body as MemberExpression;
+            if (body == null)
+            {
+                throw new InvalidOperationException("Improperly formatted expression");
+            }
+            var propertyName = body.Member.Name;
+            if (!_propertiesInfo[propertyName].Property.CanWrite)
+            {
+                throw new ReadOnlyException($"{propertyName} is readonly, please remove it from builder sequence");
+            }
+            _propertiesInfo[propertyName].Value = value;
+            return this;
+        }
+
+        public T Build(Func<T> instance)
+        {
+            T localInstance = instance();
+            PropertyInfo[] instanceProperties = localInstance.GetType().GetProperties();
+
+            PropertyInfo[] matchingProperties =
+            (from tProperty in instanceProperties
+             from localProperty in _propertiesInfo
+             where tProperty.Name == localProperty.Key &&
+               tProperty.PropertyType == localProperty.Value.Property.PropertyType
+             select localProperty.Value.Property).ToArray();
+
+            foreach (PropertyInfo p in matchingProperties)
+            {
+                if (p.CanWrite)
+                {
+                    p.SetValue(localInstance, _propertiesInfo[p.Name].Value);
+                }
+            }
+            return localInstance;
+        }
+
+        public T Build() => Build(() => (T)Activator.CreateInstance(typeof(T)));
+    }
+}
---
+++ test/GenericDesignPatterns.Samples.Tests.Unit/Creational/BuilderTests.cs
@@ -0,0 +1,101 @@
+﻿using FluentAssertions;
+using GenericDesignPatterns.Creational;
+using GenericDesignPatterns.Samples.SampleClasses;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using Xunit;
+
+namespace GenericDesignPatterns.Samples.Tests.Unit.Creational
+{
+    public class BuilderTests
+    {
+        private School _school;
+
+        public BuilderTests()
+        {
+            _school = null;
+            var schoolBuilder = new Builder<School>();
+            var managerBuilder = new Builder<Manager>();
+            var teacherBuilder = new Builder<Teacher>();
+            var students = new List<Student>();
+
+            students.Add(new Builder<Student>().SetProperty(entity => entity.Name, "Bruce")
+                .SetProperty(entity => entity.Age, 14)
+                .Build());
+
+            _school = schoolBuilder.SetProperty(entity => entity.Name, "Master School")
+                .SetProperty(entity => entity.Manager, managerBuilder.SetProperty(entity => entity.Name, "John")
+                    .SetProperty(entity => entity.Salary, 10000)
+                    .Build())
+                .SetProperty(entity => entity.Teacher, teacherBuilder.SetProperty(entity => entity.Name, "John")
+                    .SetProperty(entity => entity.ClassroomName, "Classroom 1")
+                    .SetProperty(entity => entity.Students, students)
+                    .Build())
+                .Build();
+        }
+
+        [Fact]
+        public void Should_created_school_instance_not_be_null()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+            _school.Should().NotBeNull();
+        }
+
+        [Fact]
+        public void Should_created_school_instance_name_be_correct()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+            _school.Name.Should().Be("Master School");
+        }
+
+        [Fact]
+        public void Should_created_school_instance_teacher_has_at_least_one_student()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+            _school.Teacher.Students.Count.Should().BeGreaterThan(0);
+        }
+
+        [Fact]
+        public void Should_created_school_instance_readonly_Id_property_set_value_throw_exception()
+        {
+            //Arrange
+            var schoolBuilder = new Builder<School>();
+
+            //Act
+            Action buildAction = () => schoolBuilder.SetProperty(entity => entity.Id, Guid.NewGuid()).Build();
+
+            //Assert
+
+            buildAction.Should().Throw<ReadOnlyException>()
+                .WithMessage("Id is readonly, please remove it from builder sequence");
+        }
+
+        [Fact]
+        public void Should_created_school_instance_empty_body_throw_exception()
+        {
+            //Arrange
+            var schoolBuilder = new Builder<School>();
+
+            //Act
+            Action buildAction = () => schoolBuilder.SetProperty(entity => null, new Manager()).Build();
+
+            //Assert
+            buildAction.Should().Throw<InvalidOperationException>()
+                .WithMessage("Improperly formatted expression");
+        }
+
+    }
+}
